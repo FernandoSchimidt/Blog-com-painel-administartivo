753dae9fd6efee6a658f6d32fe7a727469405445
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-03-03 13:52:25 -0300

    s
    s

--- article/ArticleController.js
+++ article/ArticleController.js
@@ -1,12 +1,36 @@
 //express router
 const express = require('express');
 const router = express.Router();
+const Category = require('../categories/Category');
+const Article = require('./Article');
+const slugify = require('slugify');
 
-router.get('/admin/articles',(req,res)=>{
-    res.send('Rota de artigos');
-});
 router.get('/admin/articles/new', (req, res) => {
-   
-    res.render('admin/articles/new'); //pagina new
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new', {
+            categories: categories
+        }); //pagina new
+
+    })
 });
-module.exports= router;
+router.get('/admin/articles', (req, res) => {
+    res.render('admin/articles/index')
+});
+//rota que grava o artigo
+router.post('/articles/save', (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    //grava no modelo
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect('/admin/articles');
+    });
+});
+
+module.exports = router;
--- database/database.js
+++ database/database.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize');
 //informação para conexao
-//const connection = new Sequelize('guiapress','root','F3rn@nd0',{ //trampo
-const connection = new Sequelize('guiapress', 'root', '172203', {
+const connection = new Sequelize('guiapress','root','F3rn@nd0',{ //trampo
+//const connection = new Sequelize('guiapress', 'root', '172203', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone:'-03:00'//ajusta a hora do brazil
 });
 
 module.exports = connection;
